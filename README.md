2bb94e5145132406186537aad334ba6444844f30
Federico Nova <fedenova8@gmail.com>
2024-12-13 13:50:20 -0300

    Creamos la primer parte, la parte A

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Nombre: Federico Nova
 
-# Link a GitHub:
+# Link a GitHub: https://github.com/feden15/desafio-09
 
-# Link a Netlify:
+# Link de Netlify: https://desafio-09.netlify.app/
--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 import './style.css'
+
+ let cantidadDeGatos = Number(prompt('Indique la cantidad de gatos'))
+/* let cantidadDePasos = Number(prompt('Indique la cantidad de pasos')) */
+
+for (let i=1; i<=cantidadDeGatos; i++) {
+  if (i % 3 == 0) {
+    console.log('GATO #' + i + ': TERCERO');
+  } else if (i % 3 == 2) {
+    console.log('GATO #' + i + ': SEGUNDO/PAR');
+  } else {
+    console.log('GATO #' + i + ': PRIMERO');
+  }
+}
